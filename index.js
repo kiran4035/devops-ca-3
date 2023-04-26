f9f76da8191a5dea0debc424f93f9568773a55b3
Kiran Kumar R <109475226+kiran4035@users.noreply.github.com>
2023-04-26 04:52:39 +0000

    ..

--- index.js
+++ index.js
@@ -12,16 +12,6 @@ app.use(cors({
     allowedHeaders: ["*"]
 }))
 
-app.use((req, res, next) => {
-    if (/(.ico|.js|.css|.jpg|.png|.map)$/i.test(req.path)) {
-        next();
-    } else {
-        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-        res.header('Expires', '-1');
-        res.header('Pragma', 'no-cache');
-        res.sendFile(path.join(__dirname, 'public', 'index.html'));
-    }
-});
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/version', (req, res) => {
