a28ac8e9b468f6d53e2ec147b3de3a99283e0684
Kiran Kumar R <109475226+kiran4035@users.noreply.github.com>
2023-04-26 11:08:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(helmet())
-app.use(cors("*"))
+app.use(cors({
+    "origin": "weather.b68dev.xyz"    
+))
 
 app.use(express.static(path.join(__dirname, 'public')));
 
